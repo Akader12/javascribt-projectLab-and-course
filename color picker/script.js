fc246bc762e34ae77a5589c55184c8fcbbafc14e
Akader12 <abdikadermohakassim@gmail.com>
2025-05-25 14:47:36 +0300

    last commit

--- color picker/script.js
+++ color picker/script.js
@@ -1,3 +1,54 @@
+// let colors = document.querySelectorAll('.color')
+// const selectedcolorHex = document.getElementById('selectedColorHex')
+// const colorBox = document.querySelector('.colorBox')
+// const customHexInput = document.getElementById('customHexInput')
+// const copyHexbtn = document.getElementById('copyHexBtn');
+// const addCustomColorBtn = document.getElementById('customColorBtn');
+// let palette = document.querySelector('.palatte');
+
+// //add event listener to thhe all predifined colr elements
+// colors.forEach(colorGet)
+// function colorGet (color) {
+//     color.addEventListener('click',e=>{
+//         const hexCode = e.target.getAttribute('data-hex')
+//         selectedcolorHex.textContent = hexCode;
+//         colorBox.style.background = hexCode;
+//     })
+// }
+
+// //copy  hexcode to clipboard
+
+// copyHexbtn.addEventListener('click',event =>{
+//     const hexCode = selectedcolorHex.textContent;
+//     navigator.clipboard.writeText(hexCode).then(()=>{
+//         alert('hexcode copied to clipboard')
+//     }).catch(error =>{
+//         alert('error copting hexcode ')
+//     })
+// })
+
+
+// //add custom color
+// addCustomColorBtn.addEventListener('click',(e)=>{
+//     const customHexCode = customHexInput.value.trim()
+
+//     //validate hexcode
+//     if(/^#[0-9A-F]{6}$/i.test(customHexCode)){
+//         const newColor = document.createElement('div');
+//         newColor.classList.add('color');
+//         newColor.style.backgroundColor = customHexCode;
+//         newColor.setAttribute('data-hex',customHexCode);
+//         palette.append(newColor);
+//         // colors = document.querySelectorAll('.color');
+//         // palette = document.querySelector('.palatte');
+//         colorGet(newColor)
+//     }else{
+//         alert('please valid hex code')
+//     }
+    
+// })
+
+
 let colors = document.querySelectorAll('.color')
 const selectedcolorHex = document.getElementById('selectedColorHex')
 const colorBox = document.querySelector('.colorBox')
@@ -6,44 +57,35 @@ const copyHexbtn = document.getElementById('copyHexBtn');
 const addCustomColorBtn = document.getElementById('customColorBtn');
 let palette = document.querySelector('.palatte');
 
-//add event listener to thhe all predifined colr elements
 colors.forEach(colorGet)
-function colorGet (color) {
-    color.addEventListener('click',e=>{
-        const hexCode = e.target.getAttribute('data-hex')
-        selectedcolorHex.textContent = hexCode;
+function colorGet(color){
+    color.addEventListener("click",e =>{
+        const hexCode = e.target.getAttribute('data-hex');
+        selectedcolorHex.innerHTML = hexCode;
         colorBox.style.background = hexCode;
     })
 }
 
-//copy  hexcode to clipboard
-
-copyHexbtn.addEventListener('click',event =>{
+copyHexbtn.addEventListener("click",e=>{
     const hexCode = selectedcolorHex.textContent;
     navigator.clipboard.writeText(hexCode).then(()=>{
-        alert('hexcode copied to clipboard')
-    }).catch(error =>{
-        alert('error copting hexcode ')
+        alert("copied")
+    }).catch(e=>{
+        alert("error")
     })
 })
-
-
-//add custom color
-addCustomColorBtn.addEventListener('click',(e)=>{
+addCustomColorBtn.addEventListener("click",e=>{
     const customHexCode = customHexInput.value.trim()
-
     //validate hexcode
     if(/^#[0-9A-F]{6}$/i.test(customHexCode)){
-        const newColor = document.createElement('div');
-        newColor.classList.add('color');
-        newColor.style.backgroundColor = customHexCode;
-        newColor.setAttribute('data-hex',customHexCode);
-        palette.append(newColor);
-        // colors = document.querySelectorAll('.color');
-        // palette = document.querySelector('.palatte');
+        const newColor = document.createElement("div");
+        newColor.classList.add("color");
+        newColor.style.background = customHexCode;
+        newColor.setAttribute("data-hex",customHexCode);
+        palette.append(newColor)
         colorGet(newColor)
     }else{
-        alert('please valid hex code')
+       alert("plaese inter valid hexcode")
     }
     
 })
